07539cc0158e47120dcbd10cae586bab29648a81
Guy Rodrigue Koffi <koffirodrigue@gmail.com>
2016-03-11 14:09:22 +0100

    config: remove unused config

--- config.py
+++ config.py
@@ -3,24 +3,4 @@
 
 from blessings import Terminal
 
-# =====> Global config
-term = Terminal()
-stack = "node"
 
-
-# stack = "nginx"
-# ===============
-
-
-# =====> Global config
-term = Terminal()
-stack = "move-gateway"
-tfstate_key = "frontend"
-ami_param = "nginx_ami"
-dns_records = [{"Name": "prd-move.aa-aws-relay.com.",
-                "Alias":
-                    "rest.axa-assistance.com / prd-move.aa-aws-relay.com"},
-               {"Name": "sandbox.aa-aws-relay.com.",
-                "Alias": "sandbox.aa-aws-relay.com"}]
-acct_number = "925434450864"
-# ===============
