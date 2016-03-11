432a6a026074b786076bba86a60e970747e468f1
Guy Rodrigue Koffi <koffirodrigue@gmail.com>
2016-03-11 13:59:39 +0100

    models: classes for aws results parsing

--- models.py
+++ models.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python
 
+from config import T
+
+
 class AMI:
 
     def __init__(self):
         self.id = None
+        self.architecture = None
+        self.block_device_mappings = []
+        self.creation_date = None
+        self.hypervisor = None
+        self.image_type = None
+        self.location = None
         self.name = None
-        self.launch_time = None
-        self.private_ip_address = None
-        self.public_ip_address = None
-        self.vpc_id = None
-        self.image_id = None
-        self.private_dns_name = None
-        self.key_name = None
-        self.subnet_id = None
-        self.instance_type = None
-        self.availability_zone = None
-        self.commit_sha = None
-        self.asg_name = None
+        self.owner_id = None
+        self.public = None
+        self.root_device_name = None
+        self.root_device_type = None
+        self.state = None
         self.tags = []
+        self.virtualization_type = None
+
+
+    @staticmethod
+    def object_with_json(json):
+        if json is None:
+            return None
+
+        o = AMI()
+        o.id = json.get('ImageId')
+        o.architecture = json.get('Architecture')
+        o.creation_date = json.get('CreationDate')
+        o.hypervisor = json.get('Hypervisor')
+        o.image_type = json.get('ImageType')
+        o.location = json.get('ImageLocation')
+        o.owner_id = json.get('OwnerId')
+        o.public = json.get('ImageLocation')
+        o.root_device_name = json.get('RootDeviceName')
+        o.root_device_type = json.get('RootDeviceType')
+        o.state = json.get('State')
+        o.virtualization_type = json.get('VirtualizationType')
+
+        o.tags = []
+        o.virtualization_type = None
+
+
 
 
 class EC2Instance:
@@ -58,7 +86,7 @@ class EC2Instance:
 
             ec2_json = ec2_json.get('Reservations')[0].get('Instances')[0]
         except Exception as e:
-            print term.red("Failed to retrieve instance details")
+            print T.red("Failed to retrieve instance details")
             print e
 
         if ec2_json is None:
@@ -92,17 +120,39 @@ class EC2Instance:
         return i
 
 
+class AWSBlockDevice:
+
+    def __init__(self):
+        self.device_name = None
+        self.snapshot_id = None
+        self.volume_size = None
+        self.volume_type = None
+        self.encrypted = None
+
+    @staticmethod
+    def object_with_json(json):
+        if json is None:
+            return None
+
+        o = AWSBlockDevice()
+        o.device_name = json.get('DeviceName')
+        o.snapshot_id = json.get('Ebs').get('SnapshotId')
+        o.volume_size = json.get('Ebs').get('VolumeSize')
+        o.volume_type = json.get('Ebs').get('VolumeType')
+        o.volume_type = json.get('Ebs').get('Encrypted')
+
+
 class AWSTag:
     def __init__(self):
         self.key = None
         self.value = None
 
     @staticmethod
-    def object_with_json(tag_json):
-        if tag_json is None:
+    def object_with_json(json):
+        if json is None:
             return None
 
-        t = AWSTag()
-        t.key = tag_json.get('Key')
-        t.value = tag_json.get('Value')
-        return t
+        o = AWSTag()
+        o.key = json.get('Key')
+        o.value = json.get('Value')
+        return o
