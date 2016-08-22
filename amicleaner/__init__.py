08679f678f0cfa776ab3786edc64ef2fa4cb57c2
Guy Rodrigue Koffi <koffirodrigue@gmail.com>
2016-08-22 13:18:41 +0200

    first pip packaging

--- amicleaner/__init__.py
+++ amicleaner/__init__.py
@@ -4,4 +4,5 @@ __title__ = 'amicleaner'
 __version__ = '0.1.0'
 __short_version__ = '.'.join(__version__.split('.')[:2])
 __author__ = 'Guy Rodrigue Koffi'
+__author_email__ = 'koffirodrigue@gmail.com'
 __license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from amicleaner import __author__, __author_email__
+from amicleaner import __license__, __version__
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
+install_requirements = ['awscli', 'argparse', 'boto',
+                        'boto3', 'prettytable', 'blessings']
+
+test_requirements = ['moto', 'pytest', 'pytest-pep8', 'pytest-cov']
+
+
+setup(
+    name='aws-amicleaner',
+    version=__version__,
+    description='Cleanup tool for AWS AMIs and snapshots',
+    long_description=readme, 
+    author=__author__,
+    author_email=__author_email__,
+    url='https://github.com/bonclay7/aws-amicleaner/',
+    license=__license__,
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'amicleaner = amicleaner.cli:main',
+        ],
+    },
+    tests_require=test_requirements,
+    install_requires=install_requirements,
+)
